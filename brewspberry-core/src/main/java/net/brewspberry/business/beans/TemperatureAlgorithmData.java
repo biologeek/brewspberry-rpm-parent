82f9b841e4dbc2038f08b6a0816a11cc38cac1db
xavier <xavier@xavier-CX61-2PC>
2016-05-22 23:44:58 +0200

    building algo

--- brewspberry-core/src/main/java/net/brewspberry/business/beans/TemperatureAlgorithmData.java
+++ brewspberry-core/src/main/java/net/brewspberry/business/beans/TemperatureAlgorithmData.java
@@ -1,5 +1,41 @@
 package net.brewspberry.business.beans;
 
+import java.util.Date;
+
 public class TemperatureAlgorithmData  extends AbstractAlgorithmData{
+	
+	
+	Date date;
+	double actualTemperatureValue;
+	
+	double[] derivedValues;
+	double[] integratedValues;
+	
+	
+	public Date getDate() {
+		return date;
+	}
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	public double getActualTemperatureValue() {
+		return actualTemperatureValue;
+	}
+	public void setActualTemperatureValue(double actualTemperatureValue) {
+		this.actualTemperatureValue = actualTemperatureValue;
+	}
+	public double[] getDerivedValues() {
+		return derivedValues;
+	}
+	public void setDerivedValues(double[] derivedValues) {
+		this.derivedValues = derivedValues;
+	}
+	public double[] getIntegratedValues() {
+		return integratedValues;
+	}
+	public void setIntegratedValues(double[] integratedValues) {
+		this.integratedValues = integratedValues;
+	}
+	
 
 }
--- brewspberry-core/src/main/java/net/brewspberry/regulation/temperature/TemperatureRegulatorAlgorithmlmpl.java
+++ brewspberry-core/src/main/java/net/brewspberry/regulation/temperature/TemperatureRegulatorAlgorithmlmpl.java
@@ -1,5 +1,8 @@
 package net.brewspberry.regulation.temperature;
 
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Date;
 import java.util.List;
 
 import net.brewspberry.business.beans.TemperatureAlgorithmData;
@@ -10,7 +13,31 @@ public class TemperatureRegulatorAlgorithmlmpl implements IRegulationAlgorithm<T
 	@Override
 	public List<TemperatureAlgorithmData> calculateVariationSpeed(
 			List<TemperatureAlgorithmData> data) {
-		// TODO Auto-generated method stub
+		
+		if (data != null && data.size() > 0){
+			
+			Collections.sort(data, new Comparator<TemperatureAlgorithmData>(){
+
+				@Override
+				public int compare(TemperatureAlgorithmData o1,
+						TemperatureAlgorithmData o2) {
+					
+					if (o1.getDate().after(o2.getDate())){
+						return 1;
+					}
+					else if (o1.getDate().before(o2.getDate())){
+							return 0;
+					}
+					else{
+						return -1;
+					}
+				}
+			});
+			
+			
+			
+		}
+		
 		return null;
 	}
 
