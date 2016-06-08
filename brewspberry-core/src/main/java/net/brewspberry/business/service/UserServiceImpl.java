361fa13f313ce2094fd2bcc9f8cd1f9608a701b7
bldksj <azerty@ytreza>
2016-06-08 23:12:04 +0200

    building connection service

--- brewspberry-core/src/main/java/net/brewspberry/business/service/UserServiceImpl.java
+++ brewspberry-core/src/main/java/net/brewspberry/business/service/UserServiceImpl.java
@@ -1,24 +1,41 @@
 package net.brewspberry.business.service;
 
-
 import java.util.List;
 
+import javax.transaction.Transactional;
+
+import net.brewspberry.business.IGenericDao;
 import net.brewspberry.business.IGenericService;
 import net.brewspberry.business.ISpecificUserService;
 import net.brewspberry.business.beans.User;
 import net.brewspberry.dao.UserDaoImpl;
 import net.brewspberry.exceptions.DAOException;
+import net.brewspberry.util.validators.UserValidator;
+import net.brewspberry.util.validators.UserValidatorErrors;
 
 public class UserServiceImpl implements IGenericService<User>, ISpecificUserService {
 
-	private UserDaoImpl userDao;
+	private IGenericDao<User> userDao;
 
 	@Override
+	@Transactional
 	public User save(User arg0) throws DAOException {
 
-		
-		
-		return null;
+		List<UserValidatorErrors> err = UserValidator.getInstance().isCompleteUserValid(arg0);
+
+		if (err.isEmpty()) {
+
+			// Everything is OK
+
+			try {
+				userDao.save(arg0);
+			} catch (DAOException e) {
+				e.printStackTrace();
+			}
+
+		}
+
+		return arg0;
 	}
 
 	@Override
@@ -30,7 +47,7 @@ public class UserServiceImpl implements IGenericService<User>, ISpecificUserServ
 	@Override
 	public User getElementById(long id) {
 		User result = new User();
-		if (id > 0){
+		if (id > 0) {
 			result = userDao.getElementById(id);
 		}
 		return result;
@@ -45,13 +62,13 @@ public class UserServiceImpl implements IGenericService<User>, ISpecificUserServ
 	@Override
 	public void deleteElement(long id) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void deleteElement(User arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
@@ -63,7 +80,8 @@ public class UserServiceImpl implements IGenericService<User>, ISpecificUserServ
 	@Override
 	public User returnUserByCredentials(String username, String notYetEncryptedPassword) {
 
-		if (!username.equals("") && !notYetEncryptedPassword.equals("")){
+		List <UserValidatorErrors> errs = UserValidator.getInstance().isUsernameAndPasswordUserValid(username, notYetEncryptedPassword);
+		if (errs.isEmpty()){
 			
 		}
 		return null;
@@ -72,10 +90,12 @@ public class UserServiceImpl implements IGenericService<User>, ISpecificUserServ
 	@Override
 	public boolean checkIfUserIsActiveAndNotBlocked(User user) {
 
-		
-		
-		return false;
-	}
+		if (user.isUs_active() && user.isUs_force_inactivated()) {
+			return true;
+		} else {
 
+			return false;
+		}
+	}
 
 }
