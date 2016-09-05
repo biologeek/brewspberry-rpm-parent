6564d807c321d1f05486ed2f81cebca1b51c11f2
bldksj <azerty@ytreza>
2016-09-05 08:15:34 +0200

    hibernate properties for test and building crud for product service

--- brewspberry-api/src/main/java/net/brewspberry/front/ws/IProductRESTService.java
+++ brewspberry-api/src/main/java/net/brewspberry/front/ws/IProductRESTService.java
@@ -1,5 +1,6 @@
 package net.brewspberry.front.ws;
 
+import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Response;
 
 import net.brewspberry.front.ws.beans.IngredientJSONRequest;
@@ -8,6 +9,10 @@ public interface IProductRESTService {
 	
 	
 	public Response addIngredient(IngredientJSONRequest request);
+	public Response updateIngredient(IngredientJSONRequest request);
+	public Response getIngredient(String type, long id);
+	public Response deleteIngredient(IngredientJSONRequest request);
+	
 	
 	
 	
--- brewspberry-api/src/main/java/net/brewspberry/front/ws/impl/ProductRESTServiceImpl.java
+++ brewspberry-api/src/main/java/net/brewspberry/front/ws/impl/ProductRESTServiceImpl.java
@@ -2,19 +2,22 @@ package net.brewspberry.front.ws.impl;
 
 import java.util.logging.Logger;
 
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 
 import net.brewspberry.business.IGenericService;
 import net.brewspberry.business.beans.AbstractIngredient;
 import net.brewspberry.business.beans.Houblon;
 import net.brewspberry.business.beans.Levure;
 import net.brewspberry.business.beans.Malt;
-import net.brewspberry.business.exceptions.ServiceException;
 import net.brewspberry.front.ws.IProductRESTService;
 import net.brewspberry.front.ws.beans.IngredientDTO;
 import net.brewspberry.front.ws.beans.IngredientJSONRequest;
@@ -23,12 +26,15 @@ import net.brewspberry.util.LogManager;
 public class ProductRESTServiceImpl implements IProductRESTService {
 
 	@Autowired
+	@Qualifier("maltServiceImpl")
 	IGenericService<Malt> maltService;
 
 	@Autowired
+	@Qualifier("hopServiceImpl")
 	IGenericService<Houblon> hopService;
 
 	@Autowired
+	@Qualifier("yeastServiceImpl")
 	IGenericService<Levure> levService;
 
 	Logger logger;
@@ -83,10 +89,50 @@ public class ProductRESTServiceImpl implements IProductRESTService {
 
 		return Response.status(200).entity(new IngredientDTO().toServiceObject(businessIngredient)).build();
 	}
+	
+	
+	@PUT
+	@Produces(MediaType.APPLICATION_JSON)
+	@Override
+	public Response updateIngredient(IngredientJSONRequest req){
+		
+		return null;
+		
+	}
+
+	@Override
+	public Response deleteIngredient(IngredientJSONRequest request) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
 
 	private boolean isThereOnlyOneIngredientInRequest(IngredientJSONRequest request) {
 
 		return true;
 	}
 
+	@Override
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response getIngredient(@PathParam("type") String type, @PathParam("id") long id) {
+
+		if (id > 0){
+			
+			switch (type.toLowerCase()){
+			case "malt":
+				
+			break;
+			case "hop" :
+			break;
+			
+			case "yeast":
+			break;
+			
+			
+			}
+			
+		}
+		return null;
+	}
 }
