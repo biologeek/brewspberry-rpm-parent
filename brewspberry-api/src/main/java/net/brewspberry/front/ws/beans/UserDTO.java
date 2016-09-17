b9cee41b4f8b521cb11042df5c4b1188a22d1c93
bldksj <azerty@ytreza>
2016-09-17 13:23:35 +0200

    files were ignored

---
+++ brewspberry-api/src/main/java/net/brewspberry/front/ws/beans/UserDTO.java
@@ -0,0 +1,24 @@
+package net.brewspberry.front.ws.beans;
+
+import net.brewspberry.business.beans.User;
+
+public class UserDTO {
+
+	public User fromFullUserToBusinessObject(UserRegisterBean bean) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	public User fromRegistrationToBusinessObject(UserRegisterBean bean) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	public UserRegisterBean toTransferObject(User user){
+		return null;
+		
+	}
+	public UserFullBean toFullBean(User user){
+		return null;		
+	}
+
+}
---
+++ brewspberry-api/src/main/java/net/brewspberry/front/ws/beans/UserLoginBean.java
@@ -0,0 +1,29 @@
+package net.brewspberry.front.ws.beans;
+
+public class UserLoginBean {
+	
+	
+	private String username;
+	private String passwrd;
+	
+	
+	
+	public UserLoginBean() {
+		super();
+	}
+	
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPasswrd() {
+		return passwrd;
+	}
+	public void setPasswrd(String passwrd) {
+		this.passwrd = passwrd;
+	}
+	
+
+}
