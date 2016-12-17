a9eb6f6dae3387989519ddeff0344e82753582d5
bldksj <azerty@ytreza>
2016-12-17 16:04:42 +0100

    v

--- brewspberry-jbatches/src/test/java/net/brewspberry/batches/test/config/BatchesSpringTestConfiguration.java
+++ brewspberry-jbatches/src/test/java/net/brewspberry/batches/test/config/BatchesSpringTestConfiguration.java
@@ -1,11 +1,13 @@
 package net.brewspberry.batches.test.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.annotation.PropertySources;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
 import net.brewspberry.test.util.config.SpringCoreTestConfiguration;
 
@@ -15,7 +17,14 @@ import net.brewspberry.test.util.config.SpringCoreTestConfiguration;
 		@ComponentScan.Filter(type = FilterType.ANNOTATION, value = Configuration.class) })
 @Import({ SpringCoreTestConfiguration.class })
 @PropertySources(value = { @PropertySource("classpath:config.test.properties"),
-		@PropertySource("classpath:c3po.properties"), @PropertySource("classpath:devices.test.properties") })
+		@PropertySource("classpath:c3po.properties"), @PropertySource("classpath:devices.test.properties")
+, @PropertySource("classpath:batches.test.properties"), @PropertySource("classpath:stock.test.properties") })
 public class BatchesSpringTestConfiguration {
 
+
+	@Bean
+	public static PropertySourcesPlaceholderConfigurer configurer() {
+		PropertySourcesPlaceholderConfigurer config = new PropertySourcesPlaceholderConfigurer();
+		return config;
+	}
 }
