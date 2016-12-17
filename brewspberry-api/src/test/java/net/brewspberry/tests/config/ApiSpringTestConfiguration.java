58229a3e8783ed40f6d426f62e914f2e8eaa8c78
bldksj <azerty@ytreza>
2016-12-17 16:24:31 +0100

    fixed

--- brewspberry-api/src/test/java/net/brewspberry/tests/config/ApiSpringTestConfiguration.java
+++ brewspberry-api/src/test/java/net/brewspberry/tests/config/ApiSpringTestConfiguration.java
@@ -6,6 +6,8 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import net.brewspberry.test.util.config.SpringCoreTestConfiguration;
@@ -17,6 +19,9 @@ import net.brewspberry.test.util.config.SpringCoreTestConfiguration;
 		@ComponentScan.Filter(type=FilterType.ANNOTATION, value=Configuration.class)
 })
 @Import(SpringCoreTestConfiguration.class)
+@PropertySources(value = { @PropertySource("classpath:config.test.properties"),
+		@PropertySource("classpath:c3po.properties"), @PropertySource("classpath:devices.test.properties")
+, @PropertySource("classpath:batches.test.properties"), @PropertySource("classpath:stock.test.properties") })
 public class ApiSpringTestConfiguration {
 
 //	
